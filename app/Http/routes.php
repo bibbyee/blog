95ead1a337aea43fefaabf784d3d2e76abfaab55
bibbyee <npower@vip.qq.com>
2016-08-15 17:49:12 +0800

    10min blog

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return redirect('/blog');
+});
+
+Route::get('blog', 'BlogController@index');
+Route::get('blog/{slug}', 'BlogController@showPost');
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+//        Model::unguard();
+        $this->call('PostTableSeeder');
+    }
+}
+
+class PostTableSeeder extends Seeder
+{
+    public function run()
+    {
+        App\Post::truncate();
+        factory(App\Post::class, 20)->create();
     }
 }
