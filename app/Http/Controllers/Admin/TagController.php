ecaaa6e34a6d6d7f90a445200f3fd18a9abd7443
bibbyee <npower@vip.qq.com>
2016-08-16 18:30:45 +0800

    admin_tab unfinish

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -6,8 +6,39 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use App\Tag;
 
 class TagController extends Controller
 {
     //
+    protected $fields = [
+        'tag' => '',
+        'title' => '',
+        'subtitle' => '',
+        'meta_description' => '',
+        'page_image' => '',
+        'layout' => 'blog.layouts.index',
+        'reverse_direction' => 0,
+    ];
+
+    public function index()
+    {
+        $tags = Tag::all();
+        return view('admin.tag.index')->withTags($tags);
+    }
+
+    public function create()
+    {
+        $data = [];
+        foreach ($this->fields as $field => $default) {
+            $data[$field] = old($field, $default);
+        }
+
+        return view('admin.tag.create', $data);
+    }
+
+    public function store()
+    {
+
+    }
 }
