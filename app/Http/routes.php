b599bb76f5c1020db572d7780242ba68b63750c3
bibbyee <npower@vip.qq.com>
2016-08-16 12:11:37 +0800

    blog admin

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,3 +21,18 @@ Route::get('/', function () {
 
 Route::get('blog', 'BlogController@index');
 Route::get('blog/{slug}', 'BlogController@showPost');
+
+// Admin area
+Route::get('admin', function () {
+    return redirect('/admin/post');
+});
+$router->group(['namespace' => 'Admin', 'middleware' => 'auth'], function () {
+    Route::resource('admin/post', 'PostController');
+    Route::resource('admin/tag', 'TagController');
+    Route::get('admin/upload', 'UploadController@index');
+});
+
+// Logging in and out
+Route::get('/auth/login', 'Auth\AuthController@getLogin');
+Route::post('/auth/login', 'Auth\AuthController@postLogin');
+Route::get('/auth/logout', 'Auth\AuthController@getLogout');
