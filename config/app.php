6cda59ef06e5be00dc537eabd3d0387e6f7b3088
bibbyee <npower@vip.qq.com>
2016-08-25 10:54:56 +0800

    add laravel debugbar

--- config/app.php
+++ config/app.php
@@ -202,6 +202,8 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        //Laravel Debugbar
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
